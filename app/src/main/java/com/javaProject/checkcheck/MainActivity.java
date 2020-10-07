d4e8a6947e303075182b6adbe5c8a6615264c2c0
Lee-Ga-Ram <dlrkfka3538>
2020-10-07 17:19:57 +0900

    login&join

--- app/src/main/java/com/javaProject/checkcheck/MainActivity.java
+++ app/src/main/java/com/javaProject/checkcheck/MainActivity.java
@@ -1,29 +1,53 @@
 package com.javaProject.checkcheck;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-
+import android.widget.EditText;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth userdata;
+    private EditText inputEmail;
+    private EditText inputPass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        userdata = FirebaseAuth.getInstance();
+        inputEmail = (EditText)findViewById(R.id.et_id);
+        inputPass = (EditText)findViewById(R.id.et_pass);
+        Button btn_login = (Button)findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String email = inputEmail.getText().toString().trim();
+                String pwd = inputPass.getText().toString().trim();
+                userdata.signInWithEmailAndPassword(email,pwd).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                            startActivity(intent);
+                        }else{
+                        }
+                    }
+                });
+            }
+        });
         Button imageButton = (Button) findViewById(R.id.btn_gojoin);
         imageButton.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
                 startActivity(intent);
-
             }
         });
     }
