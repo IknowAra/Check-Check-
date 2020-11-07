24dc2ddb6d126fbfe5cb6ab9b37a39142d852601
Lee-Ga-Ram <63588586+Lee-Ga-Ram@users.noreply.github.com>
2020-11-07 18:21:06 +0900

    Group추가

--- app/src/release/java/com/javaProject/checkcheck/Group.java
+++ app/src/release/java/com/javaProject/checkcheck/Group.java
@@ -10,7 +10,7 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class GroupActivity extends AppCompatActivity {
+public class Group extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
