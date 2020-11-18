6b9e47cc28c778bf7889aacc19b5c05d3e31794e
Lee-Ga-Ram <63588586+Lee-Ga-Ram@users.noreply.github.com>
2020-11-18 11:16:41 +0900

    드로어 색 변경

---
+++ app/src/main/java/com/javaProject/checkcheck/CalenderActivity.java
@@ -0,0 +1,32 @@
+package com.javaProject.checkcheck;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class CalenderActivity {
+    CalendarView calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calender);
+        calendar = findViewById(R.id.calender);
+
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth ) {
+                String Date = dayOfMonth + "-" + (month+1) + "-" + year;
+
+            }
+        });
+
+    }
+
+}
